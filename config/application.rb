b2b85e565bbfb65359921d42d5d57b1de06f0764
Алексей <a.krasikov@oblakogroup.ru>
2016-02-23 23:56:30 +0300

    assets fix#2

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Amicables
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << "vendor/assets"
-    config.serve_static_assets = true
   end
 end
