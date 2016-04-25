f805a23cb842fab910d842a78a4e2aabcbb552b0
Алексей <a.krasikov@oblakogroup.ru>
2016-04-25 23:56:35 +0300

    smthng with migration #4

--- db/migrate/20160318175444_create_release_images.rb
+++ db/migrate/20160318175444_create_release_images.rb
@@ -1,7 +1,7 @@
 class CreateReleaseImages < ActiveRecord::Migration
   def change
     create_table :release_images do |t|
-      t.references :releases, index: true, foreign_key: true
+      t.references :release, index: true, foreign_key: true
 
       t.timestamps null: false
     end
