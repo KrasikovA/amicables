10618956b21b1190a7885b104f9af8cb41300cb4
Алексей <a.krasikov@oblakogroup.ru>
2016-04-25 23:51:32 +0300

    smthng with migration #2

--- db/migrate/20160331014000_add_release_to_release_images.rb
+++ db/migrate/20160331014000_add_release_to_release_images.rb
@@ -1,5 +1,5 @@
 class AddReleaseToReleaseImages < ActiveRecord::Migration
   def change
-    add_column :release_images, :release_id, :reference
+    add_column :release_images, :releases_id, :reference
   end
 end
